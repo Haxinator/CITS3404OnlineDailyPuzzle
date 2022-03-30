b929b86f7752c20ae1c89723d1e99f2ac546411d
Bye0123 <102362821+Bye0123@users.noreply.github.com>
2022-03-30 20:21:28 +0800

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+document.addEventListener("contentloaded",() =>{
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form_hidden");
+        createAccountForm.classList.remove("form_hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form_hidden");
+        createAccountForm.classList.add("form_hidden");
+    });
+});
