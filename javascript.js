6dfa3cc5dda8581dc7a31ecc683156e65e140877
Haxinator <72430334+Haxinator@users.noreply.github.com>
2022-03-30 10:44:08 +0800

    Made some changes needed for merging.
    - Grid and pallet now in "dailypuzzle.html".
    - Cleaned some java script code: added white space, semicolons, and brackets; removed my old comments; added some new comments.
    - updated dailypuzzle.html and index.html so that they're the same as main.
    - updated stylesheet so it's the same as main.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
-//Constants
+//Constants //!but they change? Shouldn't they be a global var?
 COLOR = "rgb(255, 168, 168)"
 CHOSENCELLS = []
 
 
-/*                  #TODO
+/*             //!     #TODO
 create inital array or randomly generate inital array
 (what the user has to copy), it's contents will be colours
 represented as strings (as they're known in css). To make
@@ -20,9 +20,9 @@ cell.style.backgroundColor = correct[r][c];
 /*create the canvas
 #Not sure yet how large the canvas should be
 #Need to consider how well it would feel on a phone*/
-createCanvas(16, 16);
+createCanvas(8, 8);
 
-/* Multipurpose function
+/* createCanvas
 Creates a canvas with a specified number of rows and
 columns.
 @param numRows - number of rows the table will have.
@@ -31,31 +31,35 @@ function createCanvas(numRows, numColumns) {
   var canvas = document.getElementById("canvas");
   var table = document.createElement("table");
 
-
-
-
-
   for (let r = 0; r < numRows; r++) {
     var row = document.createElement("tr");
 
     for (let c = 0; c < numColumns; c++) {
       //allocating index to element in the Table
-      let index = c + numColumns * r
+      let index = c + (numColumns * r); //!added brackets for readability
       let cell = document.createElement("td");
+
       //setting box classname so we can give style
       cell.setAttribute("class", "box");
       cell.setAttribute("id", index.toString());
+
       //adding addEventListener after click change the color
       cell.addEventListener("click", () => {
+
         console.log(cell.style.backgroundColor);
+
         if (cell.style.backgroundColor == COLOR) {
+
           cell.style.backgroundColor = null;
-          CHOSENCELLS.pop(index)
+          CHOSENCELLS.pop(index);
           // console.log(CHOSENCELLS);
+
         } else {
+
           cell.style.backgroundColor = COLOR;
-          CHOSENCELLS.push(index)
+          CHOSENCELLS.push(index);
           // console.log(CHOSENCELLS);
+
         }
       });
       row.appendChild(cell);
@@ -66,18 +70,7 @@ function createCanvas(numRows, numColumns) {
   canvas.appendChild(table);
 }
 
-/*                  #TODO
-------------------------Colour pallet.---------------
-I think the best way is to make a new function similar
-to createCanvas, that assigns the each cell a colour in the second
-loop. You may need to make an array that contains the colors we'll use,
-to make your life easier. Name the function "createPallet". It may be
-helpful to the guy doing the task below you for you to add text inside
-the colour pallet cells (make them the same colour as the cell so
-they're hidden). That way he can get the innerHTML to find out what
-colour is selected. Unless you have a better way.
-
-May be helpful to read the comment on colour pallet functionality below.
+/*         //!         #TODO Add function description
 
 @param array of colours we need
 */
@@ -111,35 +104,11 @@ function createColorPallet(colorArray) {
 
   colorPallet.appendChild(table);
 }
+
 //Totally random color pallete
 //why rgb because document item return rgb if we wanna check we need rgb
 color = ["rgb(182, 255, 206)", "rgb(246, 255, 164)", "rgb(253, 215, 170)",
    "rgb(255, 168, 168)", "rgb(240, 165, 0)","rgb(228, 88, 38)",
     "rgb(27, 26, 23)"];
-createColorPallet(color)
 
-/*                  #TODO
-------------------------Colour pallet functionality.---------------
-
-I think the best way is
-to add a event listener for a 'click' event to each cell
-of the colour pallet, have it copy the color stored in the
-cell to a global variable (lets call it colourSelected).
-Add another on 'click' event this time to the cells of
-the canvas, and use the global variable colourSelected to
-assign that colour to the canvas cell on click.
-
-This should be simple, provided whoever does colour pallet does it
-the way I suggested (if not use jQuery). You should be able to assign
-a 'click' eventto each cell of the colour pallet by using the second
-loop of "createPallet". You should also be able to assign a click
-event to each cell on the canvas using the second loop in createTable.
-
-I'm pretty sure event listener have two arguments, the event and the
-function to execute when the even occurs. So you will have to make
-two functions one for the colour pallet event to assign a colour to
-colourSelected and another for assigning a colour to a canvas cell
-using colourSelected.
-
-May be helpful to read the comment on colour pallet above.
-*/
+createColorPallet(color);
