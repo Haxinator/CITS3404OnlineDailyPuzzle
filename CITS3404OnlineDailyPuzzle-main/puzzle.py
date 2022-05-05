b837d72d5583afb5caad74871d2aac391a4fa83a
Haxinator <22760978@student.uwa.edu.au>
2022-05-05 16:54:42 +0800

    removed old dir

--- CITS3404OnlineDailyPuzzle-main/puzzle.py
+++
@@ -1,7 +0,0 @@
-#imports the main applicate to this file
-#which just means it "copies" all the 
-#code that belongs to the app variable
-#in the app directory to this file.
-
-#Why? Cleaner and more organised files
-from app import app
