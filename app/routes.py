81d0422f4734fdc1a697c52aed5a67de9552bb93
Haxinator <22760978@student.uwa.edu.au>
2022-05-05 19:54:50 +0800

    redirect to login page

--- app/routes.py
+++ app/routes.py
@@ -11,26 +11,9 @@ from app.models import User
 from app.forms import LoginForm, RegistrationForm
 from werkzeug.urls import url_parse
 
-
-#Temporarily all security systems are disabled and instead of signup when user enter the username and password the information will just get pass into the database
-@app.route('/')
-@app.route('/login',methods = ['GET','POST'])
-#@login_required
-
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.password == form.password.data and user.username == form.username.data:
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('home')
-        return redirect(next_page)
-    return render_template("HTML/login.html", title='Sign In', form=form)
-
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Player': User}
 
 @app.route('/home')
 def home():
@@ -38,6 +21,8 @@ def home():
 
 @app.route('/game')
 def game():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
     return render_template("HTML/dailypuzzle.html", title = "Puzzle")
 
 @app.route('/rules')
@@ -48,9 +33,10 @@ def rules():
 def stats():
     return render_template("HTML/statistics.html", title = "Statistics")
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Player': User}
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -66,7 +52,23 @@ def register():
         return redirect(url_for('login'))
     return render_template('HTML/register.html', title='Register', form=form)
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
+#Temporarily all security systems are disabled and instead of signup when user enter the username and password the information will just get pass into the database
+@app.route('/')
+@app.route('/login',methods = ['GET','POST'])
+#@login_required
+
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.password == form.password.data and user.username == form.username.data:
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
+    return render_template("HTML/login.html", title='Sign In', form=form)
